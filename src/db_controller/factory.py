f69b6c19901fc78c719917d21c98eccb64746aea
Justin0828 <panyz24@mails.tsinghua.edu.cn>
2026-05-23 12:52:11 +0800

    fix import issue

--- src/db_controller/factory.py
+++ src/db_controller/factory.py
@@ -1,5 +1,3 @@
-from db_controller.postgresql_controller import PostgreSQLController
-from db_controller.mysql_controller import MySQLController
 from configparser import ConfigParser
 
 
@@ -8,8 +6,10 @@ def create_db_controller(target_dbms, config_path):
     config.read(config_path)
     
     if target_dbms == "postgres":
+        from db_controller.postgresql_controller import PostgreSQLController
         return PostgreSQLController.from_file(config)
     elif target_dbms == "mysql":
+        from db_controller.mysql_controller import MySQLController
         return MySQLController.from_file(config)
     else:
         raise NotImplementedError("This DBMS product is not supported yet! Only PostgreSQL and MySQL is supported now!")
